a56f83efd374dfb0599a9166c547d1d4f589cd5c
Victor Ho <victor.ho@sciencespo.fr>
2021-05-18 22:57:00 +0200

    test

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, Replication_of_Sieg_Yoon_2017
 
-makedocs(modules = [Replication_of_Sieg_Yoon_2017], sitename = "Replication_of_Sieg_Yoon_2017.jl")
+makedocs(modules = [Replication_of_Sieg_Yoon_2017], sitename = "Replication of Sieg and Yoon (2017)")
 
 deploydocs(repo = "github.com/vho97/Replication_of_Sieg_Yoon_2017.jl.git", devbranch = "docs")
